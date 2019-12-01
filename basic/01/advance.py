cdb421a904f205f41f1452091b322ddadd942419
sebastiankennedy <sebastiankennedy@foxmail.com>
2019-12-02 07:52:48 +0800

    01 Tutorial

---
+++ basic/01/advance.py
@@ -0,0 +1,7 @@
+slogan = '脸黑怪我咯'
+number = '7.8'
+unit = '张'
+sentence = '蓝票一个SSR都没有'
+print(slogan + str(int(float(number))) + unit + sentence)
+
+print('{}{}{}{}'.format(slogan, int(float(number)), unit, sentence))

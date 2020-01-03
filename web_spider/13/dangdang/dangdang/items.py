b381917de0cdf3a47010d93404a7d971aaef98e4
sebastiankennedy <sebastiankennedy@foxmail.com>
2020-01-03 15:12:37 +0800

    13 Web Spider - 定义 Item

--- web_spider/13/dangdang/dangdang/items.py
+++ web_spider/13/dangdang/dangdang/items.py
@@ -9,6 +9,7 @@ import scrapy
 
 
 class DangdangItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    author = scrapy.Field()
     pass
